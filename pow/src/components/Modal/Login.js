a8d9ade466cc40d01d2485ce22a7fe70a2741d36
yelim <choyr2016@naver.com>
2021-05-30 23:39:06 +0900

    [ADD] Login modal

---
+++ pow/src/components/Modal/Login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as s from './style';
+
+const Login = () => {
+    return(
+        <s.Center>
+            <s.Modal>
+                <s.Etc>
+
+                </s.Etc>
+                <s.Logo>
+
+                </s.Logo>
+                <s.Login>
+                    <input></input>
+                    <input></input>
+                </s.Login>
+                <s.Change>
+
+                </s.Change>
+                <input></input>
+                <div></div>
+            </s.Modal>
+        </s.Center>
+    )
+}
+
+export default Login;
---
+++ pow/src/components/Modal/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Center = styled.div`
+`
+
+export const Modal = styled.div`
+    width: 492px;
+    height: 492px;
+    border: 1px solid #707070;
+`
+
+export const Etc = styled.div`
+`
+
+export const Logo = styled.div`
+`
+
+export const Login = styled.div`
+`
+
+export const Change = styled.div`
+`
