e22d90b0b8e64f0e9721ed293151a34883e8249d
yelim <choyr2016@naver.com>
2021-05-30 23:38:18 +0900

    [ADD] react router 추가

--- pow/src/App.js
+++ pow/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Header from "./components/Header/Header";
 import Main from './components/Main/Main';
+import Login from './components/Modal/Login';
 import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Header></Header>
-      <Main></Main>
-      {/* <Club></Club> */}
-      {/* <ProjectIntroModal /> */}
+      <BrowserRouter>
+        <Route path="/" exact>
+          <Header></Header>
+          <Main></Main>
+        </Route>
+        <Route path="/Login" exact>
+          <Login></Login>
+        </Route>
+      </BrowserRouter>
     </>
   );
 };
