437c6e5f06b12e154e02b5bed2d85a0ec818c496
yelim <choyr2016@naver.com>
2021-05-31 19:45:11 +0900

    [MODIFY] 클래스형으로 바꾸기

--- pow/src/components/Header/Header.js
+++ pow/src/components/Header/Header.js
@@ -1,47 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as s from './style';
 import pow from '../Img/pow.png';
 import search from '../Img/search.png';
 
-const Header = () => {
-    return(
-        <s.Header>
-            <s.Center>
-                <s.Area>
-                    <s.PowLogo>
-                        <img src={pow}></img>
-                        <span>Project Once Writing</span>
-                    </s.PowLogo>
-                    <s.Searching>
-                        <input placeholder="검색어를 입력해주세요"></input>
-                    </s.Searching>
-                    <s.Login>
-                        <input type="button" value="Login"></input>
-                    </s.Login>
-                </s.Area>
-                <s.Area>
-                    <s.KindCenter></s.KindCenter>
-                    <s.Kind>
-                        <s.Select>
-                            <span>전공</span>
-                            <div/>
-                        </s.Select>
-                        <s.Select>
-                            <span>창체</span>
-                            <div/>
-                        </s.Select>
-                        <s.Select>
-                            <span>자율</span>
-                            <div/>
-                        </s.Select>
-                    </s.Kind>
-                    <s.State>
-                        <span>로그인을 해주세요!</span>
-                    </s.State>
-                </s.Area>
-            </s.Center>
-        </s.Header>
-    );
+class Header extends Component {
+    render() {
+        return(
+            <s.Header>
+                <s.Center>
+                    <s.Area>
+                        <s.PowLogo>
+                            <img src={pow} alt=""></img>
+                            <span>Project Once Writing</span>
+                        </s.PowLogo>
+                        <s.Searching>
+                            <input placeholder="검색어를 입력해주세요"></input>
+                        </s.Searching>
+                        <s.Login>
+                            <input type="button" value="Login"></input>
+                        </s.Login>
+                    </s.Area>
+                    <s.Area>
+                        <s.KindCenter></s.KindCenter>
+                        <s.Kind>
+                            <s.Select>
+                                <span>전공</span>
+                                <div/>
+                            </s.Select>
+                            <s.Select>
+                                <span>창체</span>
+                                <div/>
+                            </s.Select>
+                            <s.Select>
+                                <span>자율</span>
+                                <div/>
+                            </s.Select>
+                        </s.Kind>
+                        <s.State>
+                            <span>로그인을 해주세요!</span>
+                        </s.State>
+                    </s.Area>
+                </s.Center>
+            </s.Header>
+        );
+    }
 }
 
 export default Header;
--- pow/src/components/Main/Main.js
+++ pow/src/components/Main/Main.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as s from './style';
 import rosie from '../Img/rosie.JPG';
-
-const Main = () => {
-    return(
-        <s.Main>
-            <s.Center>
-                <s.Line>
-                    <s.Club>
-                        <img src={rosie}/>
-                    </s.Club>
-                    <s.Club/>
-                    <s.Club/>
-                    <s.Club/>
-                </s.Line>
-                <s.Line>
-                    <s.Club/>
-                    <s.Club/>
-                    <s.Club/>
-                    <s.Club/>
-                </s.Line>
-            </s.Center>
-        </s.Main>
-    )
+class Main extends Component {
+    render() {
+        return(
+            <s.Main>
+                <s.Center>
+                    <s.Line>
+                        <s.Club>
+                            <img src={rosie} alt=""/>
+                        </s.Club>
+                        <s.Club/>
+                        <s.Club/>
+                        <s.Club/>
+                    </s.Line>
+                    <s.Line>
+                        <s.Club/>
+                        <s.Club/>
+                        <s.Club/>
+                        <s.Club/>
+                    </s.Line>
+                </s.Center>
+            </s.Main>
+        )
+    }
 }
 
 export default Main;
