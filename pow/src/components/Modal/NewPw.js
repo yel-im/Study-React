d78ed04281ff5485180409d472038fcac23de5b1
yelim <choyr2016@naver.com>
2021-06-02 16:08:34 +0900

    [ADD] 비밀번호 변경 모달 추가

---
+++ pow/src/components/Modal/NewPw.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import * as s from './style';
+import pow from './Img/pow.png';
+
+class NewPw extends Component {
+    render() {
+        return(
+            <s.ModalCenter>
+                <s.PwModal>
+                    <div></div>
+                    <s.Logo>
+                        <img src={pow} alt=""></img>
+                    </s.Logo>
+                    <s.PwSpan>
+                        비밀번호 변경
+                    </s.PwSpan>
+                    <s.PwInput>
+                        <input className="ModalInput" placeholder="현재 비밀번호를 입력해주세요"></input>
+                    </s.PwInput>
+                    <s.NewPwInput>
+                        <input className="ModalInput" placeholder="변경할 비밀번호를 입력해주세요"></input>
+                        <input className="ModalInput" placeholder="비밀번호 확인"></input>
+                    </s.NewPwInput>
+                    <s.EnterBtn>
+                        <button className="LoginBtn" >ENTER</button>
+                    </s.EnterBtn>
+                </s.PwModal>
+            </s.ModalCenter>
+        )
+    }
+}
+
+export default NewPw;
