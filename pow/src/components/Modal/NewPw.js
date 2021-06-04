9808ecb0278f394f572b434e7aad55494d33cfbd
yelim <choyr2016@naver.com>
2021-06-05 00:48:20 +0900

    [ADD] 모달 띄우기

--- pow/src/components/Modal/NewPw.js
+++ pow/src/components/Modal/NewPw.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as s from './style';
 import pow from './Img/pow.png';
 
-class NewPw extends Component {
-    render() {
-        return(
-            <s.ModalCenter>
-                <s.PwModal>
-                    <s.Logo>
-                        <img src={pow} alt=""></img>
-                    </s.Logo>
-                    <s.PwSpan>
-                        <span>비밀번호 변경</span>
-                    </s.PwSpan>
-                    <s.PwInput>
-                        <input className="ModalInput" placeholder="현재 비밀번호를 입력해주세요"></input>
-                    </s.PwInput>
-                    <s.NewPwInput>
-                        <input className="ModalInput" placeholder="변경할 비밀번호를 입력해주세요"></input>
-                        <input className="ModalInput" placeholder="비밀번호 확인"></input>
-                    </s.NewPwInput>
-                    <s.EnterBtn>
-                        <button className="LoginBtn" >ENTER</button>
-                    </s.EnterBtn>
-                </s.PwModal>
-            </s.ModalCenter>
-        )
-    }
+const NewPw = () => {
+    return(
+        <s.ModalCenter>
+            <s.PwModal>
+                <s.Logo>
+                    <img src={pow} alt=""></img>
+                </s.Logo>
+                <s.PwSpan>
+                    <span>비밀번호 변경</span>
+                </s.PwSpan>
+                <s.PwInput>
+                    <input className="ModalInput" placeholder="현재 비밀번호를 입력해주세요"></input>
+                </s.PwInput>
+                <s.NewPwInput>
+                    <input className="ModalInput" placeholder="변경할 비밀번호를 입력해주세요"></input>
+                    <input className="ModalInput" placeholder="비밀번호 확인"></input>
+                </s.NewPwInput>
+                <s.EnterBtn>
+                    <button className="LoginBtn" >ENTER</button>
+                </s.EnterBtn>
+            </s.PwModal>
+        </s.ModalCenter>
+    )
 }
 
 export default NewPw;
