ff7d4e930fbbaf92543ce60aed60abfeb3535a3f
yelim <choyr2016@naver.com>
2021-06-05 01:12:43 +0900

    [MODIFY] 함수형 수정

--- pow/src/components/Main/Main.js
+++ pow/src/components/Main/Main.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as s from './style';
 import rosie from '../Img/rosie.JPG';
-class Main extends Component {
-    render() {
-        return(
-            <s.Main>
-                <s.Center>
-                    <s.Line>
-                        <s.Club>
-                            <img src={rosie} alt=""/>
-                        </s.Club>
-                        <s.Club/>
-                        <s.Club/>
-                        <s.Club/>
-                    </s.Line>
-                    <s.Line>
-                        <s.Club/>
-                        <s.Club/>
-                        <s.Club/>
-                        <s.Club/>
-                    </s.Line>
-                    <s.Line>
-                        <s.Club/>
-                        <s.Club/>
-                        <s.Club/>
-                        <s.Club/>
-                    </s.Line>
-                    <s.Line>
-                        <s.Club/>
-                        <s.Club/>
-                        <s.Club/>
-                        <s.Club/>
-                    </s.Line>
-                </s.Center>
-            </s.Main>
-        )
-    }
+
+const Main = () => {
+    return(
+        <s.Main>
+            <s.Center>
+                <s.Line>
+                    <s.Club>
+                        <img src={rosie} alt=""/>
+                    </s.Club>
+                    <s.Club/>
+                    <s.Club/>
+                    <s.Club/>
+                </s.Line>
+                <s.Line>
+                    <s.Club/>
+                    <s.Club/>
+                    <s.Club/>
+                    <s.Club/>
+                </s.Line>
+                <s.Line>
+                    <s.Club/>
+                    <s.Club/>
+                    <s.Club/>
+                    <s.Club/>
+                </s.Line>
+                <s.Line>
+                    <s.Club/>
+                    <s.Club/>
+                    <s.Club/>
+                    <s.Club/>
+                </s.Line>
+            </s.Center>
+        </s.Main>
+    )
 }
 
 export default Main;
